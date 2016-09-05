bc61418516d2918ffa380cd71468cf0b103a8702
xieguigang <xie.guigang@gmail.com>
2016-09-06 03:04:28 +0800

    fix the parser errors in the xref2go parser

---
+++ GeneOntology/AnnotationFile/xref2go/XrefId.vb
@@ -0,0 +1,23 @@
+﻿Imports System.Runtime.CompilerServices
+
+Namespace xref2go
+
+    Public MustInherit Class XrefId
+
+        ''' <summary>
+        ''' Parsing from raw string
+        ''' </summary>
+        ''' <param name="xrefId"></param>
+        Sub New(xrefId As String)
+
+        End Sub
+    End Class
+
+    Public Module XrefIdParser
+
+        <Extension>
+        Public Function Parse(Of uid As XrefId)(raw As String) As uid
+
+        End Function
+    End Module
+End Namespace
