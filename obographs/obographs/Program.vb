fb0b8115d53e7d4b441520b3e64bfae62610a6fe
xieguigang <xie.guigang@gmail.com>
2016-09-07 23:40:32 +0800

    create project

---
+++ obographs/obographs/Program.vb
@@ -0,0 +1,18 @@
+﻿'
+' Created by SharpDevelop.
+' User: xieguigang
+' Date: 9/7/2016
+' Time: 11:36 PM
+' 
+' To change this template use Tools | Options | Coding | Edit Standard Headers.
+'
+Module Program
+	Sub Main()
+		Console.WriteLine("Hello World!")
+		
+		' TODO: Implement Functionality Here
+		
+		Console.Write("Press any key to continue . . . ")
+		Console.ReadKey(True)
+	End Sub
+End Module
