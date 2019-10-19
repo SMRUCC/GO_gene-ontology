8266f73a88a9753ade3106a7ea5e9b1bb13cb603
xieguigang <xie.guigang@gcmodeller.org>
2019-10-19 15:55:17 +0800

    improvements of the to go mapping model

--- GO_Annotation/xref2go/ToGo.vb
+++ GO_Annotation/xref2go/ToGo.vb
@@ -1,111 +1,123 @@
 ﻿#Region "Microsoft.VisualBasic::d492fb01af0fbbaa4d87358614a4fab6, GO_gene-ontology\GeneOntology\Files\xref2go\ToGo.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Class ToGo
-    ' 
-    '         Properties: DbXrefID, FunctionAnnotation, GO_ID, Parser, xrefId
-    ' 
-    '         Function: LoadDocument, ToString
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Class ToGo
+' 
+'         Properties: DbXrefID, FunctionAnnotation, GO_ID, Parser, xrefId
+' 
+'         Function: LoadDocument, ToString
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
-Imports System.Text.RegularExpressions
 Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
-Imports Microsoft.VisualBasic.Language
 
 Namespace xref2go
 
     ''' <summary>
+    ''' External annotation map to go term.     
     ''' The reference links between the Go database and other biological database.
     ''' (Go数据库和其他的生物学数据库的相互之间的外键连接)
     ''' </summary>
-    ''' <remarks></remarks>
-    Public Class ToGo(Of uid As XrefId)
+    Public Class toGO
 
-        Public Property DbXrefID As String
-        Public Property GO_ID As String
-        Public Property FunctionAnnotation As String
+        ' Pfam:PF00001 7tm_1 > GO:G protein-coupled receptor activity ; GO:0004930
 
-        Dim __uid As uid
-
-        Public ReadOnly Property Parser As XrefIdTypes
-
-        Public ReadOnly Property xrefId As uid
-            Get
-                If __uid Is Nothing Then
-                    __uid = DbXrefID.Parse(Of uid)(Parser)
-                End If
-                Return __uid
-            End Get
-        End Property
+        ''' <summary>
+        ''' Annotation entry id, example as pfamId.
+        ''' (请注意，一个entry可能会map到好几个go term上面)
+        ''' </summary>
+        ''' <returns></returns>
+        Public Property entry As String
+        ''' <summary>
+        ''' The common name of the target <see cref="entry"/>
+        ''' </summary>
+        ''' <returns></returns>
+        Public Property name As String
+        ''' <summary>
+        ''' The name of the go term
+        ''' </summary>
+        ''' <returns></returns>
+        ''' <remarks>
+        ''' The go function annotation
+        ''' </remarks>
+        Public Property map2GO_term As String
+        ''' <summary>
+        ''' The mapped go term id
+        ''' </summary>
+        ''' <returns></returns>
+        Public Property map2GO_id As String
 
         Public Overrides Function ToString() As String
-            Return DbXrefID & " == " & GO_ID
+            Return $"Dim {entry}[{name}] As {map2GO_id} = '{map2GO_term}'"
         End Function
 
         ''' <summary>
         ''' 
         ''' </summary>
-        ''' <param name="Path"></param>
-        ''' <param name="DbXrefHead">COG/EC/MetaCyc/KEGG/Pfam/Reactome/SMART</param>
+        ''' <typeparam name="T"></typeparam>
+        ''' <param name="type">COG/EC/MetaCyc/KEGG/Pfam/Reactome/SMART etc.</param>
         ''' <returns></returns>
-        ''' <remarks></remarks>
-        Public Shared Function LoadDocument(Path As String, DbXrefHead As XrefIdTypes) As ToGo(Of uid)()
-            Dim reads = LinqAPI.Exec(Of NamedValue(Of String)) <=
- _
-                From s As String
-                In IO.File.ReadAllLines(Path)
-                Where s.First <> "!"c
-                Select s.GetTagValue(" > ", True)
+        Public Function ParseAnnotation(Of T As XrefId)(type As XrefIdTypes) As T
+            Return entry.Parse(Of T)(type)
+        End Function
 
-            Dim LQuery = LinqAPI.Exec(Of ToGo(Of uid)) <=
- _
-                From s As NamedValue(Of String)
-                In reads.AsParallel
-                Let Tokens As String() = Strings.Split(s.Value, " ; ")
-                Let annotation As String = Tokens(0)
-                Let id As String = Tokens(1)
-                Select New ToGo(Of uid) With {
-                    .DbXrefID = s.Name,
-                    .GO_ID = id,
-                    .FunctionAnnotation = annotation,
-                    ._Parser = DbXrefHead
+        Public Shared Iterator Function Parse2GO(file As String) As IEnumerable(Of toGO)
+            Dim lines As String() = file.SolveStream _
+                .LineTokens _
+                .SkipWhile(Function(line) line.First = "!"c) _
+                .ToArray
+            Dim tokens$()
+            Dim from As NamedValue(Of String)
+            Dim mapTo As NamedValue(Of String)
+            Dim mapping As toGO
+
+            For Each line As String In lines
+                tokens = line.Split(">"c)
+                from = tokens(0).GetTagValue(" ", trim:=True)
+                mapTo = tokens(1).GetTagValue(";", trim:=True)
+                mapping = New toGO With {
+                    .entry = from.Name,
+                    .name = from.Value,
+                    .map2GO_term = mapTo.Name,
+                    .map2GO_id = mapTo.Value
                 }
 
-            Return LQuery
+                Yield mapping
+            Next
         End Function
+
     End Class
 End Namespace
