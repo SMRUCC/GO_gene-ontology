cde62b672993ded34d591f7e667474b0c9f31ff8
xieguigang <xie.guigang@gcmodeller.org>
2019-10-19 17:40:36 +0800

    TODO: parse Typedef?

--- obographs/obographs/DAGModel.vb
+++ obographs/obographs/DAGModel.vb
@@ -1,11 +1,12 @@
 ﻿Imports System.Runtime.CompilerServices
 Imports Microsoft.VisualBasic.Data.visualize.Network.Graph
+Imports Microsoft.VisualBasic.Linq
 Imports SMRUCC.genomics.Data.GeneOntology.OBO
 
 Public Module DAGModel
 
     ''' <summary>
-    ''' 
+    ''' Create a <see cref="NetworkGraph"/> based on a given go term id list.
     ''' </summary>
     ''' <param name="go"></param>
     ''' <param name="terms">
@@ -14,6 +15,13 @@ Public Module DAGModel
     ''' <returns></returns>
     <Extension>
     Public Function CreateGraph(go As GO_OBO, terms As IEnumerable(Of String)) As NetworkGraph
+        Dim g As New NetworkGraph
 
+        ' 每一个term都单独构建出一条通往base namespace的途径
+        For Each termId As String In terms.SafeQuery
+
+        Next
+
+        Return g
     End Function
 End Module
